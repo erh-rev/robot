fa8c6d237395a51dd446ba9edf086aae589e5d2e
Ocramlh <79995382+Pingw157@users.noreply.github.com>
2025-01-30 15:28:26 +0100

    Create sample-code.java

---
+++ sample-code.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Disabled
+@Autonomous
+
+public class TestAutoAlpha01 {
+  static void myMethod() {
+    System.out.println("Hello World!");
+  }
+	// todo: write your code here
+}
