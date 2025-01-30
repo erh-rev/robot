c712f59ba7347795817ca9a7a7cc9d98a116e3de
gregoire123 <glegars92@gmail.com>
2025-01-30 15:06:44 +0100

    création de autonomus v1 pour marc

---
+++ autonomous_1.java
@@ -0,0 +1,41 @@
+
+private void avancer(double puissance) {
+    rouedroite.setPower(puissance);
+    rouegauche.setPower(puissance);
+}
+
+private void reculer(double puissance) {
+    rouedroite.setPower(-puissance);
+    rouegauche.setPower(-puissance);
+}
+
+private void tourner(double puissance) {
+    rouedroite.setPower(puissance);
+    rouegauche.setPower(-puissance);
+}
+@Override
+public void runOpMode() {
+    // Initialisation des moteurs et servos comme précédemment
+
+    waitForStart();
+
+    // Exemple d'utilisation des fonctions
+    while (opModeIsActive()) {
+        // Avancer pendant 2 secondes
+        avancer(0.5);
+        sleep(2000); // Avancer pendant 2000 ms (2 secondes)
+
+        // Reculer pendant 2 secondes
+        reculer(0.5);
+        sleep(2000); // Reculer pendant 2000 ms (2 secondes)
+
+        // Tourner pendant 2 secondes
+        tourner(0.5);
+        sleep(2000); //tourner pendant 2 secondes
+        // Arrêter le robot
+        rouedroite.setPower(0);
+        rouegauche.setPower(0);
+        
+        // Ajoutez d'autres logiques ou conditions si nécessaire
+    }
+}
